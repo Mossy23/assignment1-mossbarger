98432d2785289d675922ef0f8338c7156a5afc6c
Mossy23 <jm2mossy@gmail.com>
2026-09-18 10:34:16 -0400

    Add initial JavaScript code for cookie calculation

---
+++ javascript1.js
@@ -0,0 +1,11 @@
+console.log("This is javascript page 1");
+
+// Name: Joseph
+// Favorite color: Blue
+
+let cookies = 5;
+const people = 2;
+
+cookies = cookies - people;
+
+console.log(`There are currently ${cookies} cookies left`);
